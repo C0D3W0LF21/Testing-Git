ec3c4bd99019fc0668087e3f1c8e3ef1434ef01a
Michael <mihkov03@gmail.com>
2024-07-12 16:35:57 +0200

    add an array and event listener

--- code.js
+++ code.js
@@ -2,7 +2,13 @@ const body = document.querySelector("body")
 const header = document.querySelector("h1")
 const button = document.querySelector("button")
 
+const arr = ["blue", "red", "yellow", "green", "purple", "orange"]
 
+button.addEventListener('click', () => {
+  const color = arr[Math.floor(Math.random() * arr.length)]
+  
+  document.body.style.backgroundColor = color
+})
 
 
 
